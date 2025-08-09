becdcb1a29c40600f6f599ffee2f7855cf4d1ef8
cssmith8 <cssmith4509@gmail.com>
2025-08-09 18:08:24 -0500

    remove context

--- src/types/types.rs
+++ src/types/types.rs
@@ -1,4 +1,4 @@
 pub struct Data {} // User data, which is stored and accessible in all command invocations
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
-pub type Context<'a> = poise::Context<'a, Data, Error>;
+//pub type Context<'a> = poise::Context<'a, Data, Error>;
 pub type AppContext<'a> = poise::ApplicationContext<'a, Data, Error>;
